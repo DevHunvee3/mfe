dcd6ab9030760ed7bfeb80a01e909e9d76c48f25
Eduardo Nicolás Quintana <nquintana@simtlix.com>
2021-06-30 14:52:15 -0300

    fix mount

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,12 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const mount = (el) => {
-    ReactDOM.render(<App />, el);
-};
-if (process.env.NODE_ENV === "development") {
-    const mountingPoint = document.querySelector("#container-mount-point");
-    if (mountingPoint) mount(mountingPoint);
-}
-
-export { mount };
+ReactDOM.render(<App />, document.querySelector("#container-mount-point"));
