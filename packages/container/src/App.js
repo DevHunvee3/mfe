ffdc49b7116aaefe9941fedf478f3b63c1d8980b
Eduardo Nicolás Quintana <nquintana@simtlix.com>
2021-06-30 15:42:17 -0300

    fix shared styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./apps/marketing-app";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "_co_",
+});
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
 export default App;
